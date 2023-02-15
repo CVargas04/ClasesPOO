e067c324cd5a65827831309e348bc92c9ef157cf
juancamilovargasmoreno <juancamilovargasmoreno46@gmail.com>
2023-02-15 08:36:21 -0500

    Initial commit

--- EjerciciosClases/Ejercicio#1.py
+++ EjerciciosClases/Ejercicio#1.py
@@ -3,4 +3,7 @@ if __name__ == "__main__":
         def __init__(self,velocidad_maxima,kilometraje ):
             self.velocidad_maxima=velocidad_maxima
             self.kilometraje=kilometraje
-        pass
+
+carro=Vehiculo(34,56)
+print(carro.kilometraje)
+print(carro.velocidad_maxima)
